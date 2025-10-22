a6746fb854c670a9faa93dd4ed95c6703185459b
daymoove <daymoove@gmail.com>
2025-10-22 14:45:38 +0200

    Create class GenerateSurface

---
+++ Source/ProceduralGeneration/GenerateSurface.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GenerateSurface.h"
+
+
+
+// Sets default values
+AGenerateSurface::AGenerateSurface()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+
+
+
+
+
+// Called when the game starts or when spawned
+void AGenerateSurface::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+
+
+
+
+
---
+++ Source/ProceduralGeneration/GenerateSurface.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include  "ProceduralMeshComponent.h"
+#include "GenerateSurface.generated.h"
+
+UCLASS()
+class PROCEDURALGENERATION_API AGenerateSurface : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AGenerateSurface();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+
+};
